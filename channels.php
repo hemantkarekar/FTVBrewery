899fcaf8438cae6b113908478e64f2a919e66b78
Hemant Karekar <hemant.v.karekar@gmail.com>
2021-10-04 19:10:28 +0530

    last commit of 04 oct

---
+++ channels.php
@@ -0,0 +1,435 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <?php include "includes/_head.php"; ?>
+  <title>Channels | FTV Brewery</title>
+</head>
+<style type="text/css">
+  #more {
+    display: none;
+    margin-top: -58px;
+  }
+</style>
+
+<body>
+  <!-- <?php include "includes/_header.php"; ?> -->
+  <main class="channels">
+    <section></section>
+    <section class="intro"></section>
+    <section class="channel odd">
+      <div class="channel-content">
+        <div class="text-content">
+          <div class="title">
+            <h1>MIDNIGHT SECRETS</h1>
+          </div>
+          <div class="desc">
+            <p>
+              Witness the world of sensuality and seduction on the Midnight Secrets channel, where beautiful women take you on the journey of sensuality and luxury.
+              <br>
+              Watch the Midnight Secrets shows 24x7 available on our TV Network. You can also access the channel on-demand with our VOD service any time you want.
+            </p>
+          </div>
+        </div>
+        <div class="media-content">
+          <div class="video-wrap">
+            <video autoplay muted loop>
+              <source src="/assets/media/videos/Channels Videos/Bewery -1.mp4">
+            </video>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="channel even">
+      <div class="channel-content">
+        <div class="text-content">
+          <div class="title">
+            <h1>F-MEN</h1>
+          </div>
+          <div class="desc">
+            <p>
+              A premium media channel made exclusively for men.
+              <br>
+              With F-Men you can get access to the amazing shows related to
+              men's Fashion, Beauty, Fitness, Style, Grooming, Clothing, Men's
+              Fashion, and everything that revolves around the lives of men.
+            </p>
+          </div>
+        </div>
+        <div class="media-content">
+          <div class="video-wrap">
+            <video autoplay muted loop>
+              <source src="/assets/media/videos/Channels Videos/Bewery -2.mp4">
+            </video>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="channel odd">
+      <div class="channel-content">
+        <div class="text-content">
+          <div class="title">
+            <h1>Hair and Makeup</h1>
+          </div>
+          <div class="desc">
+            <p>
+              FashionTV’s Hair and Makeup provides a plethora of intriguing
+              content from the hairstyling and makeup industry.
+              <br>
+              You can view the Hair and Makeup channel any time on your TV
+              network or stream on-demand anytime you want.
+            </p>
+          </div>
+        </div>
+        <div class="media-content">
+          <div class="video-wrap">
+            <video autoplay muted loop>
+              <source src="/assets/media/videos/Channels Videos/Bewery -3.mp4">
+            </video>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="channel even">
+      <div class="channel-content">
+        <div class="text-content">
+          <div class="title">
+            <h1>FashionTV L’Original</h1>
+          </div>
+          <div class="desc">
+            <p>
+              FashionTV L’Original gives you access to premium channel
+              content, FTV fashion events like fashion weeks, calendar shoots,
+              swimwear shoots, etc. across the world straight on your TV.
+              <br>
+              You can also access the media channel on-demand from your
+              smartphone.
+            </p>
+          </div>
+        </div>
+        <div class="media-content">
+          <div class="video-wrap">
+            <video autoplay muted loop>
+              <source src="/assets/media/videos/Channels Videos/Bewery -4.mp4">
+            </video>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="channel odd">
+      <div class="channel-content">
+        <div class="text-content">
+          <div class="title">
+            <h1>Photo-Shoots</h1>
+          </div>
+          <div class="desc">
+            <p>
+              Our Photoshoots channel gives you backstage access to various
+              FashionTV photoshoots.
+            </p>
+          </div>
+        </div>
+        <div class="media-content">
+          <div class="video-wrap">
+            <video autoplay muted loop>
+              <source src="/assets/media/videos/Channels Videos/Bewery -5.mp4">
+            </video>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="channel even">
+      <div class="channel-content">
+        <div class="text-content">
+          <div class="title">
+            <h1>Fashion Films</h1>
+          </div>
+          <div class="desc">
+            <p>
+              FashionTV brings you the most curated and FINEST collection of
+              Fashion. Films of your favourite fashion designers and POPULAR
+              fashion houses around the world.
+            </p>
+          </div>
+        </div>
+        <div class="media-content">
+          <div class="video-wrap">
+            <video autoplay muted loop>
+              <source src="/assets/media/videos/Channels Videos/Bewery -6.mp4">
+            </video>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="channel odd">
+      <div class="channel-content">
+        <div class="text-content">
+          <div class="title">
+            <h1>Fashion Weeks</h1>
+          </div>
+          <div class="desc">
+            <p>
+              With the Fashion Weeks media channel, you can catch the latest
+              fashion TRENDS across the world from the top Fashion Weeks
+              destinations like New York, Milan, London, and Paris.
+            </p>
+          </div>
+        </div>
+        <div class="media-content">
+          <div class="video-wrap">
+            <video autoplay muted loop>
+              <source src="/assets/media/videos/Channels Videos/Bewery -7.mp4">
+            </video>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="channel even">
+      <div class="channel-content">
+        <div class="text-content">
+          <div class="title">
+            <h1>FashionTV Global</h1>
+          </div>
+          <div class="desc">
+            <p>
+              FashionTV gives you insights into the worldwide fashion industry at
+              your fingertips. Get the latest updates, news, and alerts about the
+              fashion world with our FashionTV Global channel.
+            </p>
+          </div>
+        </div>
+        <div class="media-content">
+          <div class="video-wrap">
+            <video autoplay muted loop>
+              <source src="/assets/media/videos/Channels Videos/Bewery -8.mp4">
+            </video>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="channel odd">
+      <div class="channel-content">
+        <div class="text-content">
+          <div class="title">
+            <h1>Bridal Fashion</h1>
+          </div>
+          <div class="desc">
+            <p>
+              With the Fashion Weeks media channel, you can catch the latest
+              fashion TRENDS across the world from the top Fashion Weeks
+              destinations like New York, Milan, London, and Paris.
+            </p>
+          </div>
+        </div>
+        <div class="media-content">
+          <div class="video-wrap">
+            <video autoplay muted loop>
+              <source src="/assets/media/videos/Channels Videos/Bewery -9.mp4">
+            </video>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="channel even">
+      <div class="channel-content">
+        <div class="text-content">
+          <div class="title">
+            <h1>Model Fitness</h1>
+          </div>
+          <div class="desc">
+            <p>
+              FashionTV’s Model Fitness is where LUXURY meets fitness. It
+              takes blood, sweat, and hard work to achieve your dream physique
+              and we are your companions in your fitness journey.
+            </p>
+          </div>
+        </div>
+        <div class="media-content">
+          <div class="video-wrap">
+            <video autoplay muted loop>
+              <source src="/assets/media/videos/Channels Videos/Bewery -10.mp4">
+            </video>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="channel odd">
+      <div class="channel-content">
+        <div class="text-content">
+          <div class="title">
+            <h1>Top Models</h1>
+          </div>
+          <div class="desc">
+            <p>
+              With FTV Top Model, you get to spectate the journey of some of the
+              most popular and successful models across the world and how they
+              played their roles in shaping the fashion industry.
+            </p>
+          </div>
+        </div>
+        <div class="media-content">
+          <div class="video-wrap">
+            <video autoplay muted loop>
+              <source src="/assets/media/videos/Channels Videos/Bewery -11.mp4">
+            </video>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="channel even">
+      <div class="channel-content">
+        <div class="text-content">
+          <div class="title">
+            <h1>FTV Paris</h1>
+          </div>
+          <div class="desc">
+            <p>
+              You can catch the latest shows, fashion weeks, photoshoots,
+              calendar shoots, after parties, behind the scenes of shows, and
+              everything about fashion that goes around in Paris.
+              <br>
+              With the rising popularity of the FashionTV+ media channel and the
+              demand of the audience, FashionTV will launch some exclusive
+              new media on the FashionTV+ platform. Media such as Men’s
+              Secrets, Lingerie, 4K Channel, VR Channel, and FashionTV China
+              will be soon added to the FashionTV+ platform.
+            </p>
+          </div>
+        </div>
+        <div class="media-content">
+          <div class="video-wrap">
+            <video autoplay muted loop>
+              <source src="/assets/media/videos/Channels Videos/Bewery -12.mp4">
+            </video>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+  <?php include("includes/_footer.php"); ?>
+</body>
+
+</html>
+
+
+
+
+
+
+
+
+</div>
+
+
+
+<script type="text/javascript">
+  function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more14");
+    var btnText = document.getElementById("myBtn");
+
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+</script>
+
+<script type="text/javascript">
+  function myFunction11() {
+    var dots11 = document.getElementById("dots11");
+    var moreText = document.getElementById("more23");
+    var btnText = document.getElementById("myBtn11");
+
+    if (dots11.style.display === "none") {
+      dots11.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots11.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+</script>
+
+<script type="text/javascript">
+  function myFunction12() {
+    var dots12 = document.getElementById("dots12");
+    var moreText = document.getElementById("more24");
+    var btnText = document.getElementById("myBtn12");
+
+    if (dots12.style.display === "none") {
+      dots12.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots12.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+</script>
+<script type="text/javascript">
+  function myFunction13() {
+    var dots13 = document.getElementById("dots13");
+    var moreText = document.getElementById("more25");
+    var btnText = document.getElementById("myBtn13");
+
+    if (dots13.style.display === "none") {
+      dots13.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots13.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+</script>
+
+
+<script type="text/javascript">
+  function myFunction1() {
+    var dots1 = document.getElementById("dots1");
+    var moreText = document.getElementById("more15");
+    var btnText = document.getElementById("myBtn1");
+
+    if (dots1.style.display === "none") {
+      dots1.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots1.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+</script>
+<script type="text/javascript">
+  function myFunction2() {
+    var dots2 = document.getElementById("dots2");
+    var moreText = document.getElementById("more16");
+    var btnText = document.getElementById("myBtn2");
+
+    if (dots2.style.display === "none") {
+      dots2.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots2.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+</script>
+
+
+<?php
+// include "includes/footer.php";
+?>
--- includes/_head.php
+++ includes/_head.php
@@ -6,11 +6,11 @@
 <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
 
-<link rel="stylesheet" type="text/css" href="./assets/css/style.css">
 <link rel="stylesheet" type="text/css" href="./assets/css/responsive.css">
 <link rel="stylesheet" type="text/css" href="./assets/css/stylesheet.css">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" type="text/css" href="./assets/css/responsive.css">
+<link rel="stylesheet" type="text/css" href="./assets/css/style.css">
 
 <link href="//db.onlinewebfonts.com/c/513e00a92551c7f365c8d2d6cb475658?family=Helvetica+Neue" rel="stylesheet" type="text/css"/>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
