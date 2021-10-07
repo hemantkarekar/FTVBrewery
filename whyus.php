307766983187153a8b39563a9af9712dd117fde9
Hemant Karekar <hemant.v.karekar@gmail.com>
2021-10-07 17:01:56 +0530

    Final Commit

--- whyus.php
+++ whyus.php
@@ -3,19 +3,38 @@
 
 <head>
   <?php include "includes/_head.php"; ?>
-  <title>Our Features | FTV Brewery</title>
+  <title>Why Us | FTV Brewery</title>
 </head>
-<style type="text/css">
-  #more {
-    display: none;
-    margin-top: -58px;
-  }
-</style>
 
 <body>
   <?php include "includes/_header.php"; ?>
-  <main class="franchise">
-
+  <main class="why-us">
+    <section class="intro">
+      <div class="wrap">
+        <video class="md" autoplay preload="auto" muted loop>
+          <source src="/assets/media/videos/Why Us/2-Brewery-Why US-Mobile Version.mp4">
+        </video>
+        <video class="lg" autoplay preload="auto" muted loop>
+          <source src="/assets/media/videos/Why Us/2-Brewery-Why US.mp4">
+        </video>
+      </div>
+    </section>
+    <section class="benefit">
+      <div class="wrap">
+        <div class="title">
+          <h1>benefits of Partnering with Us</h1>
+        </div>
+        <div class="desc">
+          <p>
+            Partnering with a global brand like FashionTV can put your name on the map. The ultimate home of luxury, fashion, lifestyle. <br>
+            FashionTV gives you 360 degree support from the day of inception of our partnership. The pre opening support consists of location analysis, architecture, staff recruitment and strategic planning. <br>
+            We also provide promotional activities planning, marketing, website listings and fashionTV sponsorship opportunities. <br>
+            By collaborating with a brand name like FashionTV, you set yourself up for success.
+          </p>
+        </div>
+      </div>
+      <div class="f-logo"><img src="/assets/media/images/ftvsmalllogo.png" alt=""></div>
+    </section>
   </main>
   <?php include "includes/_footer.php"; ?>
 </body>
